a809a3a7ce03c0b7afc0966cf83ecb1322a0527d
Therainclouds <86106758+Therainclouds@users.noreply.github.com>
2022-04-27 11:16:03 +0800

    修复VR看房事件

--- 提供的商城模板/script.js
+++ 提供的商城模板/script.js
@@ -82,209 +82,106 @@ var swiper = new Swiper(".brand-slider", {
 //     alert("身份证号无效");
 //   }
 // }
-var username = "";
 function btget() {
-  debugger;
   $.ajax({
     type: "post",
-    url: "http://localhost:3430/AddUserAjax.ashx",
+    url: "http://localhost:3430/Ajax.ashx",
     data: {
       //请求json
       Username: document.getElementById("username").value,
       Usercall: document.getElementById("usercall").value,
       Userid: document.getElementById("userid").value,
-      usersex: document.getElementById("man").value,
     },
     dataType: "json",
-    timeout: 1000,
+    timeout: 10000,
+    //请求成功
+    success: function (data) {
+      // var obj = JSON.parse(data);
+      // alert(obj);
+    },
+  });
+}
+function subget() {
+  $.ajax({
+    type: "post",
+    url: "http://localhost:3430/Ajax.ashx",
+    data: {
+      //普通单间
+      Nowdate1: document.getElementById("nowdate1").value,
+      Getday1: document.getElementById("getday1").value,
+      //豪华单间
+      Nowdate2: document.getElementById("nowdate2").value,
+      Getday2: document.getElementById("getday2").value,
+      //普通双间
+      Nowdate3: document.getElementById("nowdate3").value,
+      Getday3: document.getElementById("getday3").value,
+      //豪华双间
+      Nowdate4: document.getElementById("nowdate4").value,
+      Getday4: document.getElementById("getday4").value,
+      //贵宾套房
+      Nowdate5: document.getElementById("nowdate5").value,
+      Getday5: document.getElementById("getday5").value,
+      //总统套房
+      Nowdate6: document.getElementById("nowdate6").value,
+      Getday6: document.getElementById("getday6").value,
+    },
+    dataType: "json",
+    timeout: 10000,
     //请求成功
     success: function (data) {
       var obj = JSON.parse(data);
-      if (obj == 200) {
-        alert("添加成功");
+      if (obj == 1200) {
+        alert("预约成功！");
       }
     },
   });
-  username = document.getElementById("username").value;
 }
-//预定按钮
-// function subget() {
-//   $.ajax({
-//     type: "post",
-//     url: "http://localhost:3430/Ajax.ashx",
-//     data: {
-//       UserName:username,
-//       //普通单间
-//       RoomType:1,
-//       Nowdate1: document.getElementById("nowdate1").value,
-//       Getday1: document.getElementById("getday1").value,
-//       //豪华单间
-
-//       Nowdate2: document.getElementById("nowdate2").value,
-//       Getday2: document.getElementById("getday2").value,
-//       //普通双间
-
-//       Nowdate3: document.getElementById("nowdate3").value,
-//       Getday3: document.getElementById("getday3").value,
-//       //豪华双间
-
-//       Nowdate4: document.getElementById("nowdate4").value,
-//       Getday4: document.getElementById("getday4").value,
-//       //贵宾套房
-
-//       Nowdate5: document.getElementById("nowdate5").value,
-//       Getday5: document.getElementById("getday5").value,
-//       //总统套房
-
-//       Nowdate6: document.getElementById("nowdate6").value,
-//       Getday6: document.getElementById("getday6").value,
-//     },
-//     dataType: "json",
-//     timeout: 10000,
-//     //请求成功
-//     success: function (data) {
-//       var obj = JSON.parse(data);
-//       if (obj == 1200) {
-//         alert("预约成功！");
-//       }
-//     },
-//   });
-
-// }
-
-function subget(type) {
-  switch (type) {
-    case 1:
-      $.ajax({
-        type: "post",
-        url: "http://localhost:3430/Ajax.ashx",
-        data: {
-          UserName: username,
-          //普通单间
-          RoomType: type,
-          Nowdate1: document.getElementById("nowdate" + type).value,
-          Getday1: document.getElementById("getday" + type).value,
-        },
-        dataType: "json",
-        timeout: 1000,
-        //请求成功
-        success: function (data) {
-          var obj = JSON.parse(data);
-          if (obj == 1200) {
-            alert("预约成功！");
-          }
-        },
-      });
-      break;
-    case 2:
-      $.ajax({
-        type: "post",
-        url: "http://localhost:3430/Ajax.ashx",
-        data: {
-          UserName: username,
-          //普通单间
-          RoomType: type,
-          Nowdate2: document.getElementById("nowdate" + type).value,
-          Getday2: document.getElementById("getday" + type).value,
-        },
-        dataType: "json",
-        timeout: 10000,
-        //请求成功
-        success: function (data) {
-          var obj = JSON.parse(data);
-          if (obj == 1200) {
-            alert("预约成功！");
-          }
-        },
-      });
-      break;
-    case 3:
-      $.ajax({
-        type: "post",
-        url: "http://localhost:3430/Ajax.ashx",
-        data: {
-          UserName: username,
-          //普通单间
-          RoomType: type,
-          Nowdate3: document.getElementById("nowdate" + type).value,
-          Getday3: document.getElementById("getday" + type).value,
-        },
-        dataType: "json",
-        timeout: 10000,
-        //请求成功
-        success: function (data) {
-          var obj = JSON.parse(data);
-          if (obj == 1200) {
-            alert("预约成功！");
-          }
-        },
-      });
-      break;
-    case 4:
-      $.ajax({
-        type: "post",
-        url: "http://localhost:3430/Ajax.ashx",
-        data: {
-          UserName: username,
-          //普通单间
-          RoomType: type,
-          Nowdate4: document.getElementById("nowdate" + type).value,
-          Getday4: document.getElementById("getday" + type).value,
-        },
-        dataType: "json",
-        timeout: 10000,
-        //请求成功
-        success: function (data) {
-          var obj = JSON.parse(data);
-          if (obj == 1200) {
-            alert("预约成功！");
-          }
-        },
-      });
-      break;
-    case 5:
-      $.ajax({
-        type: "post",
-        url: "http://localhost:3430/Ajax.ashx",
-        data: {
-          UserName: username,
-          //普通单间
-          RoomType: type,
-          Nowdate5: document.getElementById("nowdate" + type).value,
-          Getday5: document.getElementById("getday" + type).value,
-        },
-        dataType: "json",
-        timeout: 10000,
-        //请求成功
-        success: function (data) {
-          var obj = JSON.parse(data);
-          if (obj == 1200) {
-            alert("预约成功！");
-          }
-        },
-      });
-      break;
-    case 6:
-      $.ajax({
-        type: "post",
-        url: "http://localhost:3430/Ajax.ashx",
-        data: {
-          UserName: username,
-          //普通单间
-          RoomType: type,
-          Nowdate6: document.getElementById("nowdate" + type).value,
-          Getday6: document.getElementById("getday" + type).value,
-        },
-        dataType: "json",
-        timeout: 10000,
-        //请求成功
-        success: function (data) {
-          var obj = JSON.parse(data);
-          if (obj == 1200) {
-            alert("预约成功！");
-          }
-        },
-      });
-      break;
-  }
+//弹窗
+//单人
+document.getElementById("zhezhao").style.display = "none";
+function tankuang() {
+  document.getElementById("zhezhao1").style.display = "";
+}
+function hidder() {
+  document.getElementById("zhezhao").style.display = "none";
+}
+//豪华单
+document.getElementById("zhezhao1").style.display = "none";
+function tankuang1() {
+  document.getElementById("zhezhao1").style.display = "";
+}
+function hidder1() {
+  document.getElementById("zhezhao1").style.display = "none";
+}
+//双人
+document.getElementById("zhezhao2").style.display = "none";
+function tankuang2() {
+  document.getElementById("zhezhao2").style.display = "";
+}
+function hidder2() {
+  document.getElementById("zhezhao2").style.display = "none";
+}
+//豪华双
+document.getElementById("zhezhao3").style.display = "none";
+function tankuang3() {
+  document.getElementById("zhezhao3").style.display = "";
+}
+function hidder3() {
+  document.getElementById("zhezhao3").style.display = "none";
+}
+//贵宾
+document.getElementById("zhezhao4").style.display = "none";
+function tankuang4() {
+  document.getElementById("zhezhao4").style.display = "";
+}
+function hidder4() {
+  document.getElementById("zhezhao4").style.display = "none";
+}
+//总统
+document.getElementById("zhezhao5").style.display = "none";
+function tankuang5() {
+  document.getElementById("zhezhao5").style.display = "";
+}
+function hidder5() {
+  document.getElementById("zhezhao5").style.display = "none";
 }
